2132992b6c475affb595cefdf4f6fe2063cb5440
Roy Alcala <alcala.rao@gmail.com>
2019-10-08 15:08:56 -0500

    with find in models_shards

--- packages/vidaliiServer/servers/tests/test.databases_models_shards.js
+++ packages/vidaliiServer/servers/tests/test.databases_models_shards.js
@@ -33,16 +33,36 @@ describe('INSERT ONE SHARD. Using databases_models_shards.testingnoshards', () =
     });
 })
 
-
 describe('FIND MULTIPLE SHARD. Using databases_models_shards.testing', () => {
-    test('.find LOCAL', async () => {
+    test('.find WITHOUT FILTER SHARDS', async () => {
         const { vidalii } = await initSever
         // let result = await vidalii.databases_models_shards
         //     .testing.insertOne({ newDoc: { branch: 'local', msg: "with branch local" } })
-        let find = await vidalii.databases_models_shards
+        let result = await vidalii.databases_models_shards
             .testing.find(
                 { selector: { _id: { $gte: null } } }
+                // { selector: { _id: { $eq: '960e9191-0ffc-4096-b753-b1e529f517bb' } } }
+                // { selector: { branch: { $eq: 'remote' } } }
             )
-        expect(result.ok).toBe(true)
+        // console.log(result)
+        // expect(find).toBe(true)
+        expect(Array.isArray(result)).toBe(true)
+    });
+
+    test('.find WITH FILTER SHARDS', async () => {
+        const { vidalii } = await initSever
+        // let result = await vidalii.databases_models_shards
+        //     .testing.insertOne({ newDoc: { branch: 'local', msg: "with branch local" } })
+        let result = await vidalii.databases_models_shards
+            .testing.find(
+                { selector: { _id: { $gte: null } } }
+                // { selector: { _id: { $eq: '960e9191-0ffc-4096-b753-b1e529f517bb' } } }
+                // { selector: { branch: { $eq: 'remote' } } }
+                ,
+                { shardsFilter: ['remote', 'local'] }
+            )
+        // console.log('result.lenght::',result)
+        // expect(find).toBe(true)
+        expect(Array.isArray(result)).toBe(true)
     });
 })
