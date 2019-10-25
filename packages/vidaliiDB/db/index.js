b0d2b6dbe1dfedc001ec0849b8768d0aabfd4007
Roy Alcala <alcala.rao@gmail.com>
2019-10-24 19:03:05 -0500

    on same

--- packages/vidaliiDB/db/index.js
+++ packages/vidaliiDB/db/index.js
@@ -3,46 +3,6 @@ const R = require('ramda')
 const fxs = require('./readInstalled')(__dirname + '/installedFXS')
 const evol = require('./evol')
 const uuidv4 = require('uuid/v4')
-// const indexesInMemory = () => [
-//     [
-//         'indexesInMemory',
-//         ({ db, input }) => new Promise((resolve, reject) => {
-//             var databaseInMemory = []
-//             var defaultIndex = {
-//                 _id: {}
-//             }
-//             db.main.createReadStream()
-//                 .on('data', function (data) {
-//                     console.log('Buffers:', 'key:', data.key, 'value:', data.value)
-//                     data.value._id = data.key
-//                     defaultIndex._id = databaseInMemory.push(data.value)
-//                 })
-//                 .on('error', function (err) {
-//                     console.log('Oh my!', err)
-//                 })
-//                 .on('close', function () {
-//                     console.log('Stream closed')
-//                 })
-//                 .on('end', function () {
-//                     console.log('Stream ended')
-//                     // resolve(store)
-//                 })
-//         })
-//     ],
-//     [
-//         'test_indexes',
-//         async ({ indexesInMemory }) => {
-//             let a = await indexesInMemory
-//             console.log('indexes::', a)
-//         }
-//     ],
-//     [
-//         'checkPointers',
-//         ({ input }) => {
-//             // console.log(input)
-//         }
-//     ],
-// ]
 
 const fxsToProcess = [
     [
@@ -50,7 +10,7 @@ const fxsToProcess = [
         () => ({
             db: {
                 dir: __dirname + '/data',
-                name: 'test3'
+                name: 'test1'
             }
         }),
 
@@ -75,45 +35,7 @@ const fxsToProcess = [
     [
         'addCRUD',
         ({ db, build_response }) => ({
-            // insertOneWithPromise: ({ _id = uuidv4(), ...data }) => new Promise((resolve, reject) => {
-            //     const duplicateDoc = () => {
-            //         resolve(
-            //             build_response({
-            //                 error: {
-            //                     msg: `Error.The _id:${_id}, already exist.`
-            //                 }
-            //             })
-            //         )
-            //     }
-            //     const insertNewDoc = () => {
-            //         db.main.put(_id, data, function (err) {
-            //             if (err) {
-            //                 resolve(
-            //                     build_response({
-            //                         error: {
-            //                             msg: 'Error on addCRUD->insertOne->insertNewDoc:', err
-            //                         }
-            //                     })
-            //                 )
-            //             } else {
-            //                 resolve(
-            //                     build_response({
-            //                         _rev: 'revision here'
-            //                     })
-            //                 )
-            //             }
-            //         })
-            //     }
-
-            //     db.main.get(_id, function (response) {
-            //         R.ifElse(
-            //             R.isNil,
-            //             duplicateDoc,
-            //             insertNewDoc
-            //         )(response)
-            //     })
-
-            // }),
+         
             insertOne: async ({ _id = uuidv4(), ...data }) => {
                 const checkIfExist = async ({ _id }) => {
                     try {
--- packages/vidaliiDB/db/installedFXS/dbSelectType/index.js
+++ packages/vidaliiDB/db/installedFXS/dbSelectType/index.js
@@ -1,9 +1,26 @@
 const dbTypes = require('./readInstalled')(__dirname + '/installed')
+var fs = require('fs')
+const R = require('ramda')
+// var path = require('path');
+//Extract the filename, but leave the file extension:
+
 
 module.exports = (nameType = 'level', opt = {}) => ({ input }) => {
     const { db } = input
-    const dirMain = db.dir + '/' + db.name + '/main'
-    const dirRev = db.dir + '/' + db.name + '/rev'
+    // db.dir create dir if doesn exist, 
+    // db.name create dir if doenst exist
+    // let pathDB = path.dirname(db.dir) + '/' + db.name
+    const pathDB = db.dir + '/' + db.name
+    if (!fs.existsSync(db.dir)) {
+        fs.mkdirSync(db.dir);
+    }
+    if (!fs.existsSync(pathDB)) {
+        fs.mkdirSync(pathDB);
+    }
+    // const dirMain = db.dir + '/' + db.name + '/main'
+    // const dirRev = db.dir + '/' + db.name + '/rev'
+    const dirMain = pathDB + '/main'
+    const dirRev = pathDB + '/rev'
     return {
         main: dbTypes[nameType](dirMain, opt),
         rev: dbTypes[nameType](dirRev, opt)
