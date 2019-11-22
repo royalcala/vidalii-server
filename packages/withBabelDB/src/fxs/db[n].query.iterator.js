ee1cdd4d8368c481aa354524a2278d77e827a1a9
Roy Alcala <alcala.rao@gmail.com>
2019-11-22 12:02:01 -0600

    with conditions insted of ifs in iterator

--- packages/withBabelDB/src/fxs/db[n].query.iterator.js
+++ packages/withBabelDB/src/fxs/db[n].query.iterator.js
@@ -1,4 +1,6 @@
 import { mergeDeepRight, cond, allPass, propEq, all, equals } from 'ramda'
+const millions = n => 1000000 * n
+var howMany = millions(10)
 const initDefaultOptions = ({ options }) => {
     var defaults = {
         //other on link https://github.com/Level/leveldown#leveldown_get
@@ -13,37 +15,99 @@ const initDefaultOptions = ({ options }) => {
 }
 // const condBoth = [({ options }) => all(equals(true))(options.keys, options.values),
 const condBoth = [({ options }) => allPass([propEq('keys', true), propEq('values', true)])(options),
-({ iterator, end }) => ({
-    next: () => new Promise((resolve, reject) => {
+// ({ iterator, end }) => ({
+//     next: () => new Promise((resolve, reject) => {
+//         iterator.next((error, key, value) => {
+//             resolve({ key, value })
+//         })
+//     }),
+//     end
+// })
+async ({ iterator, onData, endCallback }) => {
+    const next = iterator => new Promise((resolve, reject) => {
         iterator.next((error, key, value) => {
-            resolve({ key, value })
+            if (error) {
+                console.log(error)
+                reject(error)
+            }
+            else {
+                resolve({ key, value })
+            }
         })
-    }),
-    end
-})
+    })
+    for (var i = 0; i < howMany; i++) {
+        var result = await next(iterator)
+        if (result.key === undefined || onData(result)) {
+            iterator.end(endCallback)
+            break
+        }
+    }
+}
 ]
 // const condOnlyKeys = [({ options }) => equals(true, options.keys),
 const condOnlyKeys = [({ options }) => allPass([propEq('keys', true), propEq('values', false)])(options),
-({ iterator, end }) => ({
-    next: () => new Promise((resolve, reject) => {
+// ({ iterator, end }) => ({
+//     next: () => new Promise((resolve, reject) => {
+//         iterator.next((error, key, value) => {
+//             resolve({ key, value })
+//         })
+//     }),
+//     end
+// })
+async ({ iterator, onData, endCallback }) => {
+    const next = iterator => new Promise((resolve, reject) => {
         iterator.next((error, key, value) => {
-            resolve({ key, value })
+            if (error) {
+                console.log(error)
+                reject(error)
+            }
+            else {
+                resolve(key)
+            }
         })
-    }),
-    end
-})
+    })
+    for (var i = 0; i < howMany; i++) {
+        var key = await next(iterator)
+        if (key === undefined || onData(result)) {
+            iterator.end(endCallback)
+            break
+        }
+    }
+}
 ]
 // const condOnlyValues = [({ options }) => equals(true, options.values),
 const condOnlyValues = [({ options }) => allPass([propEq('keys', false), propEq('values', true)])(options),
-({ iterator, end }) => ({
-    next: () => new Promise((resolve, reject) => {
+// ({ iterator, end }) => ({
+//     next: () => new Promise((resolve, reject) => {
+//         iterator.next((error, key, value) => {
+//             resolve({ key, value })
+//         })
+//     }),
+//     end
+// })
+async ({ iterator, onData, endCallback }) => {
+    const next = iterator => new Promise((resolve, reject) => {
         iterator.next((error, key, value) => {
-            resolve({ key, value })
+            if (error) {
+                console.log(error)
+                reject(error)
+            }
+            else {
+                resolve(value)
+            }
         })
-    }),
-    end
-})
+    })
+    for (var i = 0; i < howMany; i++) {
+        var value = await next(iterator)
+        if (value === undefined || onData(result)) {
+            iterator.end(endCallback)
+            break
+        }
+    }
+}
 ]
+
+
 
 const initIterator = ({ db, options = {} }) => {
     var iterator = db.iterator(options)
@@ -108,8 +172,7 @@ const initIterator = ({ db, options = {} }) => {
     // }
 
 }
-const millions = n => 1000000 * n
-var howMany = millions(10)
+
 // const onData = result => {
 //     if (result === 'holamundo'
 //         // + String(howMany - 1)
@@ -120,8 +183,25 @@ var howMany = millions(10)
 //         return true
 //     }
 // }
+
 export default (nameDB) => ({ db }) => async ({ onData = () => { }, decoderOut = true }, options = {}) => {
     var defaultOptions = initDefaultOptions({ options })
+    var iterator = db[nameDB].iterator(defaultOptions)
+    return cond([
+        condBoth,
+        condOnlyKeys,
+        condOnlyValues
+    ])({
+        options: { ...defaultOptions },
+        iterator,
+        onData,
+        decoderOut,
+        endCallback: e => {
+            if (e)
+                console.log('Error ending iterator::', e)
+        }
+    })
+
     var iterator = initIterator({
         db: db[nameDB],
         options: { ...defaultOptions }
