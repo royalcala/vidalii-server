428787fde553676cf9b2932d5228460057253081
Roy Alcala Ortiz <alcala.rao@gmail.com>
2020-02-11 13:36:22 -0600

    ...

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -5,7 +5,10 @@ import 'typeface-roboto';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+{/* <Router><App /></Router> */}
+ReactDOM.render(
+React.createElement('div',{},'HI')
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
