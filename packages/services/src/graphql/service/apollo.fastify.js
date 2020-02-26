455cf34199aa6a5be017602eaae833cc6bc37e49
Roy Alcala Ortiz <alcala.rao@gmail.com>
2020-02-26 11:20:55 -0600

    in apollo.fastify

--- packages/services/src/graphql/service/apollo.fastify.js
+++ packages/services/src/graphql/service/apollo.fastify.js
@@ -2,24 +2,88 @@ import { directives, scalars, sdls } from './loadFiles'
 import context from './context'
 const { ApolloServer } = require('apollo-server-fastify')
 
+/*
+estructure of array elements: 
+module.exports = {
+    sdl: `scalar JSON`,
+    resolver: { JSON: GraphQLJSON }
+}
+sdls:
+must to be extend Query, and extend Mutation
+*/
 
+// scalars: [
+//     fromFramework1, fromFramework2,
+//     fromCustomReadPath
+// ]
+const reducer = data => data.reduce(
+    (acc, element) => {
+        return {
+            sdl: acc.sdl.concat('\n', element.sdl),
+            resolver: { ...acc.resolver, ...element.resolver }
+        }
+    },
+    {
+        sdl: '',
+        resolver: {}
+    }
+)
 
-
-
-
-export default ({ typeDefs, resolvers, schemaDirectives, context } = {}) => {
+export default ({ scalars: [], directives: [], sdls: [], types: [], queries: [], mutations: [] }) => {
+    const Scalar = reducer(scalars)
+    const Directive = reducer(directives)
+    const Sdl = reducer(sdls)
+    const Types = reducer(types)
+    const Queries = reducer(queries)
+    const Mutations = reducer(mutations)
     return new ApolloServer({
         typeDefs: `
-        ${directives.sdls}
-        ${scalars.sdls}
-        ${sdls}
+        ${Directive.sdl}
+        ${Scalar.sdl}
+        ${Types.sdl}
+        ${Sdl.sdl}
+            type Query{
+            ${Queries.sdl}
+            hellow:String
+            }
+            type Mutation{
+            ${Mutations.sdl}
+            hellow:String
+            }
         `,
         resolvers: {
-            ...scalars.resolvers,
+            ...Scalar.resolver,
+            ...Types.resolver,
+            Query: {
+                hellow: () => 'world!',
+                ...Queries.resolver,
+            },
+            Mutation: {
+                hellow: () => 'world!',
+                ...Mutations.resolver,
+            }
         },
         schemaDirectives: {
-            ...directives.resolvers
+            ...Directive.resolver
         },
         context
     })
 }
+
+
+// export default ({ typeDefs, resolvers, schemaDirectives, context } = {}) => {
+//     return new ApolloServer({
+//         typeDefs: `
+//         ${directives.sdls}
+//         ${scalars.sdls}
+//         ${sdls}
+//         `,
+//         resolvers: {
+//             ...scalars.resolvers,
+//         },
+//         schemaDirectives: {
+//             ...directives.resolvers
+//         },
+//         context
+//     })
+// }
---
+++ packages/services/src/graphql/service/loadPath.js
@@ -0,0 +1,35 @@
+const glob = require("glob")
+const fs = require('fs-extra')
+export const loadModules = path => glob.sync(path).reduce(
+    (acc, path) => {
+        let data = require(path)
+        return {
+            sdl: acc.sdl.concat('\n' + data.sdl),
+            resolver: { ...acc.resolver, ...data.resolver }
+        }
+    }, {
+    sdl: '',
+    resolver: {}
+})
+
+export const loadGraphqls = path => glob.sync(path)
+    .reduce((acc, path) => {
+        let str = fs.readFileSync(path, 'utf8').toString()
+        return {
+            sdl: acc.sdl.concat('\n' + str),
+            resolver: {}
+        }
+    }, {
+        sdl: '',
+        resolver: {}
+    })
+
+
+
+// export const directives = loadFiles(`src/gql/directives/*.js`)
+// export const scalars = loadFiles(`src/gql/scalars/*.js`)
+// export const sdls = glob.sync(`src/gql/sdl/*.graphql`)
+//     .reduce((acc, path) => {
+//         let str = fs.readFileSync(path, 'utf8').toString()
+//         return acc.concat('\n', str)
+//     }, '')
