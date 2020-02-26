a7c25fd4f0bbb8fad8f832250c8b11954b319ae9
Roy Alcala Ortiz <alcala.rao@gmail.com>
2020-02-25 20:41:54 -0600

    sitesUnion and cache; before finish

---
+++ packages/router/src/gql/directives/cache.js
@@ -0,0 +1,26 @@
+import { defaultFieldResolver } from "graphql";
+const { SchemaDirectiveVisitor } = require('apollo-server-fastify')
+const fs = require('fs-extra')
+const name = 'cache'
+module.exports = {
+    sdl: `directive @${name}(path:String cache: Boolean=false) on FIELD_DEFINITION`,
+    resolver: {
+        [name]: class defaultNameExtended extends SchemaDirectiveVisitor {
+            visitFieldDefinition(field) {
+                const { path, cache } = this.args;
+                const blockToPath = 'uploads/' + path
+                const { resolve = defaultFieldResolver } = field;
+                field.resolve = async function () {
+                    try {
+                        const data = fs.readJsonSync(blockToPath)
+                        return data
+                    } catch (error) {
+                        return {
+                            error
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
--- packages/router/src/services/server.js
+++ packages/router/src/services/server.js
@@ -22,7 +22,6 @@ const start = async () => {
     }
 }
 
-
 export default start
 
 
